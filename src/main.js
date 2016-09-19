50c0c4afe91e7a53b398d72a4e6785d0058bd134
tomoyuen <wisyue@yahoo.com>
2016-09-19 19:08:08 +0800

    some code test;

--- src/main.js
+++ src/main.js
@@ -1,5 +1,102 @@
+/* global window, document */
 /* eslint no-console: off */
-import { WebGLRenderTargetCube } from 'three';
+import {
+  Scene,
+  Fog,
+  PerspectiveCamera,
+  WebGLRenderer,
+  HemisphereLight,
+  DirectionalLight,
+} from 'three';
 
-console.log(WebGLRenderTargetCube);
-console.log('asdfasdf');
+const Colors = {
+  red: 0xf25346,
+  white: 0xd8d0d1,
+  brown: 0x59332e,
+  pink: 0xF5986E,
+  brownDark: 0x23190f,
+  blue: 0x68c3c0,
+};
+
+function init() {
+  createScene();
+  createLights();
+  createPlane();
+  createSea();
+  createSky();
+  loop();
+};
+
+let scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH, renderer, container;
+
+function handleWindowResize() {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.aspect = WIDTH / HEIGHT;
+  camera.updateProjectionMatrix();
+}
+
+function createScene() {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+
+  scene = new Scene();
+
+  scene.fog = new Fog(0xf7d9aa, 100, 950);
+
+  aspectRatio = WIDTH / HEIGHT;
+  fieldOfView = 60;
+  nearPlane = 1;
+  farPlane = 10000;
+  camera = new PerspectiveCamera(
+    fieldOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane
+  );
+
+  camera.position.x = 0;
+  camera.position.z = 200;
+  camera.position.y = 100;
+
+  renderer = new WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  });
+
+  renderer.setSize(WIDTH, HEIGHT);
+
+  renderer.shadowMap.enabled = true;
+
+  container = document.getElementById('world');
+  container.appendChild(renderer.domElement);
+
+  window.addEventListener('resize', handleWindowResize, false);
+}
+
+let hemisphereLight, shadowLight;
+function createLights() {
+  hemisphereLight = new HemisphereLight(0xaaaaaa, 0x000000, 0.9);
+
+  shadowLight = new DirectionalLight(0xffffff, 0.9);
+
+  shadowLight.position.set(150, 350, 350);
+
+  shadowLight.castShadow = true;
+
+  shadowLight.shadow.camera.left = -400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = -400;
+  shadowLight.shadow.camera.near = 1;
+  shadowLight.shadow.camera.far = 1000;
+
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+
+  scene.add(hemisphereLight);
+  scene.add(shadowLight);
+}
+
+window.addEventListener('load', init, false);
