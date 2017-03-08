dc5a0b64264ece249662c414e745b8509d362c8a
tomoyuen <wisyue@yahoo.com>
2017-03-09 01:13:39 +0800

    make some methods to change store

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import EnnemiesHolder from './modules/EnnemiesHolder';
 import Particle from './modules/Particle';
 import ParticlesHolder from './modules/ParticlesHolder';
 
-import { game, deltaTime, ennemiesPool, particlesPool } from './config';
+import { game, deltaTime, ennemiesPool, particlesPool, resetGame, updateTime } from './config';
 
 // game variables
 var newTime = new Date().getTime(),
@@ -44,71 +44,6 @@ var newTime = new Date().getTime(),
   levelCircle,
   sea,
   sky;
-
-function resetGame() {
-  game = {
-    speed: 0,
-    initSpeed: 0.00035,
-    baseSpeed: 0.00035,
-    targetBaseSpeed: 0.00035,
-    incrementSpeedByTime: 0.0000025,
-    incrementSpeedByLevel: 0.000005,
-    distanceForSpeedUpdate: 100,
-    speedLastUpdate: 0,
-
-    distance: 0,
-    ratioSpeedDistance: 50,
-    energy: 100,
-    ratioSpeedEnergy: 3,
-
-    level: 1,
-    levelLastUpdate: 0,
-    distanceForLevelUpdate: 1000,
-
-    planeDefaultHeight: 100,
-    planeAmpHeight: 80,
-    planeAmpWidth: 75,
-    planeMoveSensivity: 0.005,
-    planeRotXSensivity: 0.0008,
-    planeRotZSensivity: 0.0004,
-    planeFallSpeed: 0.001,
-    planeMinSpeed: 1.2,
-    planeMaxSpeed: 1.6,
-    planeSpeed: 0,
-    planeCollisionDisplacementX: 0,
-    planeCollisionSpeedX: 0,
-
-    planeCollisionDisplacementY: 0,
-    planeCollisionSpeedY: 0,
-
-    seaRadius: 600,
-    seaLength: 800,
-
-    wavesMinAmp: 5,
-    wavesMaxAmp: 20,
-    wavesMinSpeed: 0.001,
-    wavesMaxSpeed: 0.003,
-
-    cameraFarPos: 500,
-    cameraNearPos: 150,
-    cameraSensivity: 0.002,
-
-    coinDistanceTolerance: 15,
-    coinValue: 3,
-    coinsSpeed: 0.5,
-    coinLastSpawn: 0,
-    distanceForCoinsSpawn: 100,
-
-    ennemyDistanceTolerance: 10,
-    ennemyValue: 10,
-    ennemiesSpeed: 0.6,
-    ennemyLastSpawn: 0,
-    distanceForEnnemiesSpawn: 50,
-
-    status: 'playing',
-  };
-  fieldLevel.innerHTML = Math.floor(game.level);
-}
 
 function createScene() {
   deviceHeight = window.innerHeight;
@@ -321,21 +256,21 @@ function handleTouchMove(event) {
 
 function handleMouseUp() {
   if (game.status === 'waitingReplay') {
-    resetGame();
+    resetGame(fieldLevel);
     hideReplay();
   }
 }
 
 function handleTouchEnd() {
   if (game.status === 'waitingReplay') {
-    resetGame();
+    resetGame(fieldLevel);
     hideReplay();
   }
 }
 
 function loop() {
   newTime = new Date().getTime();
-  deltaTime = newTime - oldTime;
+  updateTime(oldTime, newTime);
   oldTime = newTime;
   if (game.status === 'playing') {
     if (Math.floor(game.distance) % game.distanceForCoinsSpawn === 0
@@ -408,7 +343,7 @@ function init() {
   replayMessage = document.getElementById('replayMessage');
   levelCircle = document.getElementById('levelCircleStroke');
 
-  resetGame();
+  resetGame(fieldLevel);
   createScene();
 
   console.log(game);
